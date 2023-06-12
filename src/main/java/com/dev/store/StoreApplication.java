160019248acc2721be1fc009c21b4a46c06c5155
mperusso <matheus-perusso@hotmail.com>
2023-06-12 14:45:09 -0300

    Initial commit

---
+++ src/main/java/com/dev/store/StoreApplication.java
@@ -0,0 +1,13 @@
+package com.dev.store;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.*;
+
+@SpringBootApplication
+public class StoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StoreApplication.class, args);
+	}
+
+}
