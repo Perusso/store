c25447b5a798117a75fccc78bc24c7c4f293d610
mperusso <matheus-perusso@hotmail.com>
2023-06-28 22:38:40 -0300

    feat: Authenticator Rest Controller, Registration Body model

---
+++ src/main/java/com/dev/store/api/model/RegistrationBody.java
@@ -0,0 +1,11 @@
+package com.dev.store.api.model;
+
+public class RegistrationBody {
+
+    private String username;
+    private String password;
+    private String email;
+    private String firstName;
+    private String lastName;
+
+}
